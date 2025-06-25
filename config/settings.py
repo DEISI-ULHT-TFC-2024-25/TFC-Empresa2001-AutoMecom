4931122b218d37fdffe3627a6e28e7863d8fa647
Beatriz Neves <beatrizmneves@gmail.com>
2025-06-25 23:02:20 +0100

    final 3

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-q1fh-r_1)4rs!*$he82)-=h09xuhd8hdv*dl1suae!3idtac!*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["automecom.com", "www.automecom.com", "127.0.0.1"]
+ALLOWED_HOSTS = [" 85.10.135.82", "automecom.com", "www.automecom.com", "127.0.0.1"]
 
 # Application definition
 
